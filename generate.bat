f439654ae0f508944ced46ba68d83d3ce4cd43e4
iran <ma1kelvdh@gmail.com>
2013-09-24 22:27:25 +0200

    add COPYING GPL license file

--- generate.bat
+++ generate.bat
@@ -1,8 +1,7 @@
-@echo off
 for %%A in (*.ini) do (
-call RAFullMapPreviewGenerator.exe %%A --drawvisibleonly
+call RAFullMapPreviewGenerator.exe "%%A" --drawvisibleonly
 )
 
 for %%A in (*.mpr) do (
-call RAFullMapPreviewGenerator.exe %%A --drawvisibleonly
+call RAFullMapPreviewGenerator.exe "%%A" --drawvisibleonly
 )
